99db928719011bb929def2227101d86c6dbb81f7
Benny Zlotnik <bzlotnik@protonmail.com>
2024-07-31 12:58:38 +0300

    broadcast

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -26,11 +28,35 @@ pub enum Body {
     },
     #[serde(rename = "init_ok")]
     InitOk { in_reply_to: usize },
+
+    #[serde(rename = "generate")]
+    Generate { msg_id: usize },
+    #[serde(rename = "generate_ok")]
+    GenerateOk { id: String, in_reply_to: usize },
+    #[serde(rename = "broadcast")]
+    Broadcast { msg_id: usize, message: usize },
+    #[serde(rename = "broadcast_ok")]
+    BroadcastOk { in_reply_to: usize },
+    #[serde(rename = "read")]
+    Read { msg_id: usize },
+    #[serde(rename = "read_ok")]
+    ReadOk {
+        in_reply_to: usize,
+        messages: HashSet<usize>,
+    },
+    #[serde(rename = "topology")]
+    Topology {
+        msg_id: usize,
+        topology: HashMap<String, Vec<String>>,
+    },
+    #[serde(rename = "topology_ok")]
+    TopologyOk { in_reply_to: usize },
 }
 
 pub struct Node {
     pub id: String,
     pub node_ids: Vec<String>,
+    pub seen: HashSet<usize>,
 }
 
 impl Node {
@@ -38,6 +64,7 @@ impl Node {
         Node {
             id: String::new(),
             node_ids: vec![],
+            seen: HashSet::new(),
         }
     }
 
@@ -63,6 +90,45 @@ impl Node {
                     src: self.id.to_string(),
                     dest: msg.src,
                     body: Body::InitOk {
+                        in_reply_to: msg_id,
+                    },
+                })
+            }
+            Body::Generate { msg_id } => {
+                let id = format!("{}-{}", self.id, msg_id);
+                Some(Message {
+                    src: self.id.to_string(),
+                    dest: msg.src,
+                    body: Body::GenerateOk {
+                        id,
+                        in_reply_to: msg_id,
+                    },
+                })
+            }
+            Body::Broadcast { msg_id, message } => {
+                self.seen.insert(message);
+                Some(Message {
+                    src: self.id.to_string(),
+                    dest: msg.src,
+                    body: Body::BroadcastOk {
+                        in_reply_to: msg_id,
+                    },
+                })
+            }
+            Body::Read { msg_id } => Some(Message {
+                src: self.id.to_string(),
+                dest: msg.src,
+                body: Body::ReadOk {
+                    in_reply_to: msg_id,
+                    messages: self.seen.clone(),
+                },
+            }),
+            Body::Topology { msg_id, topology } => {
+                self.node_ids = topology.get(&self.id).unwrap().clone();
+                Some(Message {
+                    src: self.id.to_string(),
+                    dest: msg.src,
+                    body: Body::TopologyOk {
                         in_reply_to: msg_id,
                     },
                 })
